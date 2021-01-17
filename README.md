166cb7cd51f4f7c4f940c790d6e5fc6cd2f79446
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2021-01-17 11:34:10 -0300

    Finished homepage

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # institucional-website
-  creation of institutional website to train css and html semantic tags.
+  Creation of institutional website to train css and html semantic tags.
+  In this project, the design and images part was created in an authorial way, in addition to coding, being fully responsive.
---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+/*jQuery rolagem suave da página */
+var $doc = $('html, body');
+$(".softScroll").click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, "href") ).offset().top
+    }, 700);
+    return false;
+});
+
+function setMenuStateMobile(){
+    if(window.innerWidth<=1000){
+        if(document.getElementById('menuM').checked == false){
+            let statusM = document.querySelector(".homeSide");
+            statusM.style.display = "none";
+            statusM = document.querySelector(".slide");
+            statusM.style.display = "none";
+            statusM = document.querySelector("#about");
+            statusM.style.display = "none";
+            statusM = document.querySelector("#clients");
+            statusM.style.display = "none";
+            statusM = document.querySelector("#services");
+            statusM.style.display = "none";
+            statusM = document.querySelector("#contact");
+            statusM.style.display = "none";
+            statusM = document.querySelector("footer");
+            statusM.style.display = "none";
+            statusM = document.querySelector(".itemsMenuBar");
+            statusM.style.display = "flex";
+        } else {
+            let statusM = document.querySelector(".homeSide");
+            statusM.style.display = "flex";
+            statusM = document.querySelector(".slide");
+            statusM.style.display = "flex";
+            statusM = document.querySelector("#about");
+            statusM.style.display = "block";
+            statusM = document.querySelector("#clients");
+            statusM.style.display = "block";
+            statusM = document.querySelector("#services");
+            statusM.style.display = "block";
+            statusM = document.querySelector("#contact");
+            statusM.style.display = "block";
+            statusM = document.querySelector("footer");
+            statusM.style.display = "flex";
+            statusM = document.querySelector(".itemsMenuBar");
+            statusM.style.display = "none";
+
+        }   
+    }
+}
